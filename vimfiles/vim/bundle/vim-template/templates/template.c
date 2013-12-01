3ec27fe5abfac56c396e2f39d90febd5ccf28272
Xiangli Liao <liaoxl2012@gmail.com>
2013-12-01 14:27:23 +0800

    All my files

---
+++ vimfiles/vim/bundle/vim-template/templates/template.c
@@ -0,0 +1,15 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	%HERE%
+	printf("Hello, world!\n");
+	return 0;
+}
---
+++ vimfiles/vim/bundle/vim-template/templates/template.cpp
@@ -0,0 +1,16 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	%HERE%
+	return 0;
+}
